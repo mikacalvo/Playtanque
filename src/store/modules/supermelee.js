7712e0c9369b1e90edb8630eda9c0872e941996a
mikacalvo <mikacalvo@gmail.com>
2017-05-05 16:23:53 +0200

    tirage des adversaires super-mêlée
    TODO : essayer d'optimiser pour avoir tolérance 0 au max

--- src/store/modules/supermelee.js
+++ src/store/modules/supermelee.js
@@ -1,6 +1,6 @@
 var threshold = 0
 var thresholdTeams = 0
-var thresholdMatchs = 0
+var thresholdGames = 0
 
 // initial state
 // const state =
@@ -53,7 +53,7 @@ const getters = {
     }
     return combinaisons
   },
-  getFromUniqueness: state => () => {
+  getTeamsFromUniqueness: state => () => {
     let correspondances = []
     for (let group of state.players) {
       for (let player of group) {
@@ -75,6 +75,30 @@ const getters = {
     } else {
       return false
     }
+  },
+  getGamesFromUniqueness: state => () => {
+    return false
+    // let correspondances = []
+    // for (let group of state.players) {
+    //   for (let player of group) {
+    //     correspondances.push(player)
+    //   }
+    // }
+    // let nbplayers = state.players[0].length * state.nbGroups
+    // let uniqueComb = state.uniqueness.filter(function (obj) {
+    //   return obj.nb === nbplayers
+    // })
+    // if (uniqueComb.length > 0) {
+    //   return uniqueComb[0].teams.map((round) => {
+    //     return round.map((team) => {
+    //       return team.map((player) => {
+    //         return correspondances[player]
+    //       })
+    //     })
+    //   })
+    // } else {
+    //   return false
+    // }
   },
   uniqueTeams: state => (tolerance) => {
     var ret = false
@@ -190,12 +214,10 @@ const getters = {
               addUsed(used[i], comb[k])
               break
             } else if (typeof comb[k + 1] === 'undefined') {
-              threshold++
               return false
             }
           }
           if (equipes[i][j].length < 3) {
-            threshold++
             return false
           }
         }
@@ -204,104 +226,135 @@ const getters = {
     }
     while (ret === false && threshold < state.players[0].length * 100) {
       ret = get()
+      threshold++
     }
     return ret
   },
-  uniqueMatchs: state => (teams) => {
-    console.log('uniqueMatchs', thresholdMatchs)
-    var cumul = []
+  uniqueGames: state => (teams, tolerance) => {
+    var ret = false
+    var res = false
+
     var matchs = []
+    var playingAgainst = []
     var tmpEquipes = teams.slice(0)
-    var numeroMatch = 0
-    var advExist = true
-    var seuilTour = 0
-    var equipeIndex = 0
-    var i, l, k, tmpCumul, key
-    for (i = 0; i < state.nbRounds; i++) {
-      matchs[i] = []
-      numeroMatch = 0
-      matchs[i][numeroMatch] = []
-      matchs[i][numeroMatch].push(tmpEquipes[i][0])
-      // console.log("ajout adversaire", i, numeroMatch, tmpEquipes[i][0])
-      tmpEquipes[i].splice(0, 1)
-      advExist = false
-      getters.shuffle(tmpEquipes[i])
-      equipeIndex = 0
-      while (tmpEquipes[i].length > 0) {
-        if (typeof matchs[i][numeroMatch] === 'undefined') { // La première équipe peut être n'importe laquelle
-          matchs[i][numeroMatch] = []
-          matchs[i][numeroMatch].push(tmpEquipes[i][equipeIndex])
-          // console.log("ajout adversaire", i, numeroMatch, tmpEquipes[i][equipeIndex])
-          tmpEquipes[i].splice(equipeIndex, 1)
-          advExist = true
-        } else { // Pour la deuxième
-          for (l = 0; l < 3; l++) { // on teste pour les 3 joueurs qu'ils ne sont pas déjà tombés sur les adversaires de l'autre équipe (paire)
-            for (k = 0; k <= i; k++) { // On regarde les tours d'avant et l'actuel
-              if (
-                threshold < 20 &&
-                typeof cumul[tmpEquipes[i][equipeIndex][l]] !== 'undefined' &&
-                typeof cumul[tmpEquipes[i][equipeIndex][l]]['against'][k] !== 'undefined'
-              ) {
-                tmpCumul = (cumul[tmpEquipes[i][equipeIndex][l]]['against'][k].indexOf(matchs[i][numeroMatch][0][0]) !== -1) ? 1 : 0
-                tmpCumul += (cumul[tmpEquipes[i][equipeIndex][l]]['against'][k].indexOf(matchs[i][numeroMatch][0][1]) !== -1) ? 1 : 0
-                tmpCumul += (cumul[tmpEquipes[i][equipeIndex][l]]['against'][k].indexOf(matchs[i][numeroMatch][0][2]) !== -1) ? 1 : 0
-                if (tmpCumul > Math.floor(state.nbRounds / 2)) {
-                  advExist = true
-                  break
-                }
-              }
-            }
-            if (advExist) {
-              equipeIndex++
-              if (equipeIndex >= tmpEquipes[i].length) { // On n'a pas trouvé de combinaison
-                if (seuilTour++ < 20) {
-                  // on réessaye en remélangeant les équipes pour ce tour
-                  tmpEquipes[i] = teams[i].slice(0)
-                  matchs[i] = []
-                  matchs[i][numeroMatch] = []
-                  matchs[i][numeroMatch].push(tmpEquipes[i][0])
-                  tmpEquipes[i].splice(0, 1)
-                  // On remet à zéro les adversaires de ce tour
-                  for (key in cumul) {
-                    if (cumul.hasOwnProperty(key)) {
-                      cumul[key]['against'][i] = []
-                    }
-                  }
-                  numeroMatch = 0
-                  getters.shuffle(tmpEquipes[i])
-                  equipeIndex = 0
-                } else {
-                  // Au bout d'un moment on recommence tout le processus
-                  return thresholdMatchs++ < 20 ? getters.uniqueMatchs(teams) : false
-                }
-              }
-              break
+    var roundIterator
+    var thresholdRound = 0
+
+    function countIndexesOf (arr, player) {
+      let index = arr.indexOf(player)
+      let counter = 0
+      while (index >= 0) {
+        index = arr.indexOf(player, index + 1)
+        counter++
+      }
+      return counter
+    }
+
+    function isUsed (rounds, teamIndex) {
+      for (let i = 0; i < rounds.length; i++) {
+        if (rounds[i].indexOf(teamIndex) !== -1) {
+          // console.log('team ' + teamIndex + ' joue déjà sur ce tour')
+          return true
+        }
+      }
+      return false
+    }
+
+    function testPlayingAgainst (players) {
+      console.log('testPlayingAgainst ' + tolerance)
+      for (let i = 0; i < players.length; i++) {
+        for (let j = players.length - 1; j >= 0; j--) {
+          if (j !== i) {
+            console.log(countIndexesOf(playingAgainst[players[i]], players[j]) + '>' + tolerance)
+            if (countIndexesOf(playingAgainst[players[i]], players[j]) > tolerance) {
+              // console.log(players[i] + ' joue déjà ' + (tolerance + 1) + ' fois contre joueur ' + players[j])
+              return true
             }
           }
         }
-        if (advExist === false) {
-          matchs[i][numeroMatch].push(tmpEquipes[i][equipeIndex])
-          for (l = 0; l < 3; l++) {
-            if (typeof cumul[tmpEquipes[i][equipeIndex][l]] === 'undefined') {
-              cumul[tmpEquipes[i][equipeIndex][l]] = []
-              cumul[tmpEquipes[i][equipeIndex][l]]['against'] = []
-            }
-            cumul[tmpEquipes[i][equipeIndex][l]]['against'][i] = matchs[i][numeroMatch][0]
-            if (typeof cumul[matchs[i][numeroMatch][0][l]] === 'undefined') {
-              cumul[matchs[i][numeroMatch][0][l]] = []
-              cumul[matchs[i][numeroMatch][0][l]]['against'] = []
-            }
-            cumul[matchs[i][numeroMatch][0][l]]['against'][i] = matchs[i][numeroMatch][1]
-          }
-          numeroMatch++
-          tmpEquipes[i].splice(equipeIndex, 1)
-          equipeIndex = 0
-        } else {
-          advExist = false
+      }
+      return false
+    }
+
+    function addPlayingAgainst (players, opponents) {
+      for (let i = 0; i < opponents.length; i++) {
+        for (let j = players.length - 1; j >= 0; j--) {
+          playingAgainst[players[i]].push(opponents[j])
+          playingAgainst[opponents[j]].push(players[i])
         }
       }
     }
-    return matchs
+
+    function getValidRound (teams) {
+      let opponentIndex
+      let roundGames = []
+      let numeroMatch = 0
+      let equipeIndex = 0
+      while (equipeIndex < teams.length) {
+        equipeIndex = 0
+        console.log('numeroMatch ' + numeroMatch)
+        if (typeof roundGames[numeroMatch] === 'undefined') { // La première équipe peut être n'importe laquelle non déjà utilisée
+          while (isUsed(matchs.concat(roundGames), equipeIndex)) {
+            equipeIndex++
+          }
+          if (equipeIndex < teams.length) {
+            roundGames[numeroMatch] = []
+            roundGames[numeroMatch].push(equipeIndex)
+            // console.log('ajout equipe ' + equipeIndex, teams[equipeIndex])
+          }
+        } else { // Pour la deuxième, on reteste tout le monde
+          opponentIndex = 0
+          while (opponentIndex < teams.length) {
+            if (!isUsed(matchs.concat(roundGames), opponentIndex)) {
+              if (!testPlayingAgainst(teams[opponentIndex])) {
+                // console.log('ajout adversaire ' + opponentIndex, teams[opponentIndex])
+                roundGames[numeroMatch].push(opponentIndex)
+                addPlayingAgainst(teams[roundGames[numeroMatch][0]], teams[opponentIndex])
+                numeroMatch++
+                break
+              }
+            }
+            opponentIndex++
+          }
+          if (opponentIndex === teams.length) { // Si on n'a pas trouvé une paire, il faut tester de nouvelles combinaisons
+            return false
+          }
+        }
+      }
+      return roundGames
+    }
+
+    function get () {
+      matchs = []
+      playingAgainst = []
+      for (var t = 0; t < state.players.length; t++) {
+        for (var tt = 0; tt < state.players[t].length; tt++) {
+          playingAgainst[state.players[t][tt]] = []
+        }
+      }
+      for (roundIterator = 0; roundIterator < state.nbRounds; roundIterator++) {
+        console.log('TOUR ' + roundIterator)
+        matchs[roundIterator] = []
+        res = false
+        while (res === false && thresholdRound < 5) { // state.players[0].length * 100
+          getters.shuffle(tmpEquipes[roundIterator])
+          res = getValidRound(tmpEquipes[roundIterator])
+          thresholdRound++
+        }
+        if (res) {
+          matchs[roundIterator] = res
+        } else {
+          return false
+        }
+      }
+      return matchs
+    }
+    while (ret === false && threshold < 5) { // state.players[0].length * 100
+      ret = get()
+      threshold++
+    }
+    console.log(playingAgainst)
+    return ret
   },
   shuffle (a) {
     for (let i = a.length; i; i--) {
@@ -374,7 +427,7 @@ const actions = {
   initSupermelee ({ state, commit, getters, dispatch }) {
     console.log('initSupermelee')
     thresholdTeams = 0
-    var equipes = getters.getFromUniqueness()
+    var equipes = getters.getTeamsFromUniqueness()
     console.log(equipes)
     while (equipes === false && thresholdTeams < state.players[0].length) {
       threshold = 0
@@ -384,16 +437,14 @@ const actions = {
     if (thresholdTeams === 1) {
       dispatch('saveUniqueness', equipes)
     }
-    // var matchs = getters.getUniqueMatchs(equipes)
-    // if (matchs) {
-    //   // paintMatchs(matchs)
-    //   thresholdMatchs = 0
-    //   threshold = 0
-    // } else {
-    //   thresholdMatchs = 0
-    //   threshold++
-    //   dispatch('setConcours')
-    // }
+    thresholdGames = 0
+    var games = getters.getGamesFromUniqueness()
+    while (games === false && thresholdGames < 2) {
+      threshold = 0
+      console.log('TOLERANCE ' + thresholdGames)
+      games = getters.uniqueGames(equipes, thresholdGames++)
+    }
+    console.log(games)
   },
 
   updateSupermeleeGame ({ state, commit, dispatch }, { tournament, round, game, index, value }) {
